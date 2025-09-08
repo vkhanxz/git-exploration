ab5131e7df6c4fcb82db9012e99e768c43c8fdc0
127EAhamed <127eahamed@frhsd.com>
2025-09-08 10:34:42 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my name is Vania.");
+        System.out.println("Hi Vania, I'm Ehan");
     }
 }
